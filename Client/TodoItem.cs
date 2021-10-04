d88540682042feaa00f38c49312a79751d0fd4e8
phantomleak <dstocking0823@gmail.com>
2021-10-04 18:18:05 -0400

    added ipo functions

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -2,6 +2,6 @@
 
 public class TodoItem
 {
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+    public string Title { get; set; } // getter and setter for title of todo item
+    public bool IsDone { get; set; }  // getter and setter for the boolean IsDone
 }
